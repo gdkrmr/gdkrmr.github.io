37292ae51c340bef07e96e92ecf9b3f97c9a0c35
Guido Kraemer <gdkrmr@users.noreply.github.com>
2020-07-13 17:02:05 +0200

    update website

---
+++ publication/kraemer-dimred-2018/auto/cite.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "kraemer_dimred_2018"))
+ :bibtex)
+
---
+++ publication/kraemer-low-2020/auto/cite.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "cite"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "kraemer_low_2020"))
+ :bibtex)
+
