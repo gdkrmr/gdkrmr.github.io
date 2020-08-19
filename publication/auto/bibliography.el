1ea5f84a78656b89b60c9d8c693eff9c14e774cc
Guido Kraemer <gdkrmr@users.noreply.github.com>
2020-08-19 19:55:15 +0200

    rebuild

--- publication/auto/bibliography.el
+++ publication/auto/bibliography.el
@@ -2,6 +2,14 @@
  "bibliography"
  (lambda ()
    (LaTeX-add-bibitems
-    "mahecha_chapter_2020"))
+    "angert_contribution_2012"
+    "angert_internal_2012"
+    "kraemer_dimred_2018"
+    "kraemer_low_2020"
+    "kraemer_summarizing_2020"
+    "mahecha_chapter_2020"
+    "mahecha_earth_2020"
+    "muhr_carbon_2013"
+    "sierra_monitoring_2017"))
  :bibtex)
 
